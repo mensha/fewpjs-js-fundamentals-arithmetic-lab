0f21d6173e0f24f031e51f975e0bd9fbb00117bc
mensha <msksalston@gmail.com>
2021-05-05 20:50:05 -0400

    commit tree e31c07cf9d6b15de55ef79c9d4c54a6740252382

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+// const chai = require('chai')
+// const expect = chai.expect;
+
+let num1 = 2;
+let num2 = 31;
+const multiply = (num1 * num2)
+const random = Math.floor(9) 
+let num3 = 66
+let num4 = 31
+const mod = (num3 % num4)
+let num5 = 4
+let num6 = 6
+let num7 = 9
+let num8 = 20
+let num9 = 15
+const max = Math.max(num6, num7, num8, num9, num5)
